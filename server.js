4a1f38cc8279a7131e062df41997fec3a691102d
beatnikbrew <beatnikbrew@gmail.com>
2017-06-16 21:17:01 -0400

    trying langues without extra module

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const port = process.env.PORT || 8080;
+
+var app = express();
+
+app.get('/api', (req, res) => {
+  resObject = {};
+  resObject.Languages = req.acceptsLanguages();
+  res.send(resObject);
+});
+
+app.listen(port, () => {
+  console.log("listening on " + port);
+});
